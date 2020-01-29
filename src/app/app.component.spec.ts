fc835004e4b4e970e67539111586cc5bafb9e93e
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-29 22:25:01 +0100

    Clear out example code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('agari-angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('agari-angular app is running!');
-  });
 });
