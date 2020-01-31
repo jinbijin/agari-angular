8696eab09a136e8e854fc9a00c899123c495b195
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-31 21:18:36 +0100

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,10 @@ import { GraphQLModule } from './graphql/graphql.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule
+    GraphQLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
