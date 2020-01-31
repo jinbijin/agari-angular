caea54055da1d13d7148091ff5d1f13d89a6c253
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-31 23:52:55 +0100

    Add tslint rule

--- src/app/agari.component.spec.ts
+++ src/app/agari.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AgariComponent } from './agari.component';
 
 describe('AgariComponent', () => {
