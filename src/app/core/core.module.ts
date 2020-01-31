24f3172dbef1cbf0390a84ae6eb8b1a851cfb610
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-31 22:41:19 +0100

    Set up Prettier

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    HttpClientModule
-  ],
-  exports: [
-    BrowserAnimationsModule,
-    FlexLayoutModule
-  ],
+  imports: [CommonModule, RouterModule, BrowserAnimationsModule, FlexLayoutModule, HttpClientModule],
+  exports: [BrowserAnimationsModule, FlexLayoutModule],
   providers: []
 })
-export class CoreModule { }
+export class CoreModule {}
