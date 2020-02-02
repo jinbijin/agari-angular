0cb874ff88b46128fc116cb321f70805c92f07cd
Jinbi Jin <jinbijin@jinbijin.nl>
2020-02-02 17:38:01 +0100

    Update cypress tests

--- cypress/integration/landing.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Agari', () => {
-  it('opens the page', () => {
-    cy.visit('http://localhost:4242');
-    cy.title().should('equal', 'Agari');
-    cy.get('header').within(() => cy.get('a').contains('アガリ'));
-  });
-});
