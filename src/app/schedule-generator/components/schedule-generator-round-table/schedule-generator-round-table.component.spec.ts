68bb67f4fa9dde65b0d1662b2a74928e430c8ca5
Jinbi Jin <jinbijin@jinbijin.nl>
2020-02-23 23:19:39 +0100

    Add unit tests for RoundTable

--- src/app/schedule-generator/components/schedule-generator-round-table/schedule-generator-round-table.component.spec.ts
+++ src/app/schedule-generator/components/schedule-generator-round-table/schedule-generator-round-table.component.spec.ts
@@ -1,7 +1,7 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { MatTableModule } from '@angular/material/table';
-import { ScheduleRound } from 'src/app/graphql/generated/types';
+import { ScheduleGame, ScheduleRound } from 'src/app/graphql/generated/types';
 import { PageBase } from 'src/app/instrumentation/test/page-base';
 
 import { ScheduleGeneratorRoundTableComponent } from './schedule-generator-round-table.component';
@@ -19,11 +19,63 @@ describe('ScheduleGeneratorRoundTableComponent', () => {
 
   beforeEach(() => {
     page = new Page(TestBed.createComponent(TestHostComponent));
-    page.detectChanges();
   });
 
   it('should create', () => {
+    page.hostComponent.scheduleRound = {
+      games: []
+    };
+    page.detectChanges();
+
     expect(page.rootComponent).toBeTruthy();
+  });
+
+  it('should compute the correct table configuration', () => {
+    page.hostComponent.scheduleRound = {
+      games: [
+        { participantNrs: [1, 2, 3, 4] },
+        { participantNrs: [5, 6, 7, 8] }
+      ]
+    };
+    page.detectChanges();
+
+    expect(page.rootComponent.tableConfiguration.headers).toEqual(false);
+    expect(page.rootComponent.tableConfiguration.dataSource.data).toEqual([
+      [0, { participantNrs: [1, 2, 3, 4] }],
+      [1, { participantNrs: [5, 6, 7, 8] }]
+    ]);
+  });
+
+  it('should compute the correct column configuration', () => {
+    page.hostComponent.scheduleRound = {
+      games: [{ participantNrs: [1, 2, 3, 4] }]
+    };
+    page.detectChanges();
+
+    const element: [number, ScheduleGame] = [
+      1,
+      { participantNrs: [5, 6, 7, 8] }
+    ];
+
+    expect(page.rootComponent.tableConfiguration.columns.length).toEqual(6);
+    expect(
+      page.rootComponent.tableConfiguration.columns[0].cell(element)
+    ).toEqual('Table');
+    expect(
+      page.rootComponent.tableConfiguration.columns[1].cell(element)
+    ).toEqual(2);
+    expect(
+      page.rootComponent.tableConfiguration.columns[2].cell(element)
+    ).toEqual(5);
+    expect(
+      page.rootComponent.tableConfiguration.columns[3].cell(element)
+    ).toEqual(6);
+    expect(
+      page.rootComponent.tableConfiguration.columns[4].cell(element)
+    ).toEqual(7);
+    expect(
+      page.rootComponent.tableConfiguration.columns[5].cell(element)
+    ).toEqual(8);
   });
 });
 
@@ -47,7 +99,5 @@ class Page extends PageBase<TestHostComponent> {
   `
 })
 class TestHostComponent {
-  public scheduleRound: ScheduleRound = {
-    games: []
-  };
+  public scheduleRound: ScheduleRound;
 }
