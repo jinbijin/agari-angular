adf192da2f65597ed2369decd7b234988c40f033
Jinbi Jin <jinbijin@jinbijin.nl>
2020-02-23 15:32:11 +0100

    Test AgariTableComponent

--- src/app/shared/table/agari-table.component.spec.ts
+++ src/app/shared/table/agari-table.component.spec.ts
@@ -3,6 +3,11 @@ import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {
+  MatHeaderCellHarness,
+  MatHeaderRowHarness,
+  MatRowHarness
+} from '@angular/material/table/testing';
 import { TableConfiguration } from 'src/app/instrumentation/data/table-configuration.type';
 import { PageBase } from 'src/app/instrumentation/test/page-base';
 
@@ -25,12 +30,65 @@ describe('AgariTableComponent', () => {
 
   it('should create', () => {
     page.component.tableConfiguration = {
-      dataSource: new MatTableDataSource<any>([{ name: 'test' }]),
-      columns: [{ id: 'test', cell: element => element.name }]
+      dataSource: new MatTableDataSource<any>(),
+      columns: []
     };
     page.detectChanges();
 
     expect(page.root).toBeTruthy();
+  });
+
+  it('should have default settings', async () => {
+    page.component.tableConfiguration = {
+      dataSource: new MatTableDataSource<any>([{ name: 'test' }]),
+      columns: [{ id: 'test', header: 'Test', cell: element => element.name }]
+    };
+    page.detectChanges();
+
+    const headerRows = await page.loader.getAllHarnesses(MatHeaderRowHarness);
+    expect(headerRows.length).toBe(1);
+    const headerCells = await page.loader.getAllHarnesses(MatHeaderCellHarness);
+    expect(headerCells.length).toBe(1);
+    const rows = await page.loader.getAllHarnesses(MatRowHarness);
+    expect(rows.length).toBe(1);
+  });
+
+  it('should have optional headers', async () => {
+    page.component.tableConfiguration = {
+      dataSource: new MatTableDataSource<any>([{ name: 'test' }]),
+      headers: false,
+      columns: [{ id: 'test', cell: element => element.name }]
+    };
+    page.detectChanges();
+
+    const headerRows = await page.loader.getAllHarnesses(MatHeaderRowHarness);
+    expect(headerRows.length).toBe(0);
+    const headerCells = await page.loader.getAllHarnesses(MatHeaderCellHarness);
+    expect(headerCells.length).toBe(0);
+    const rows = await page.loader.getAllHarnesses(MatRowHarness);
+    expect(rows.length).toBe(1);
+  });
+
+  it('should have optional columns', async () => {
+    page.component.tableConfiguration = {
+      dataSource: new MatTableDataSource<any>([{ name: 'test' }]),
+      columns: [
+        {
+          id: 'test',
+          header: 'Test',
+          cell: element => element.name,
+          displayed: false
+        }
+      ]
+    };
+    page.detectChanges();
+
+    const headerRows = await page.loader.getAllHarnesses(MatHeaderRowHarness);
+    expect(headerRows.length).toBe(1);
+    const headerCells = await page.loader.getAllHarnesses(MatHeaderCellHarness);
+    expect(headerCells.length).toBe(0);
+    const rows = await page.loader.getAllHarnesses(MatRowHarness);
+    expect(rows.length).toBe(1);
   });
 });
 
