00fbc799e18c0c3056cf6018bec38593d477573e
jinbijin <35583057+jinbijin@users.noreply.github.com>
2020-03-04 23:15:27 +0100

    Add error handling (#4)
    * Add changelog
    
    * Implement global error handling
    
    * Update version
    
    * Update changelog
    
    * Add end-to-end testing

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# 1.0.1
+
+### Bug fixes
+
+- Handle errors with snackbar
+
+# 1.0.0
+
+### Features
+
+- Generate schedules randomly
--- cypress/integration/schedule-generation.cy.ts
+++ cypress/integration/schedule-generation.cy.ts
@@ -4,7 +4,7 @@ describe('Schedule generator', () => {
     cy.route(
       'POST',
       'http://localhost:5001/graphql',
-      'fixture:generate-schedule-response.json'
+      'fixture:generate-schedule/response-success.json'
     );
 
     cy.visit('http://localhost:4242/schedule-generator');
@@ -14,5 +14,22 @@ describe('Schedule generator', () => {
     cy.get('[data-test-id="request-button"]').click();
 
     cy.get('[data-test-id="schedule-round"]').should('have.length', 4);
+  });
+
+  it('should show a snackbar if no schedule is generated', () => {
+    cy.server();
+    cy.route(
+      'POST',
+      'http://localhost:5001/graphql',
+      'fixture:generate-schedule/response-graphql-error.json'
+    );
+
+    cy.visit('http://localhost:4242/schedule-generator');
+
+    cy.get('[data-test-id="round-input"]').type('9');
+    cy.get('[data-test-id="participant-input"]').type('36');
+    cy.get('[data-test-id="request-button"]').click();
+
+    cy.get('.mat-simple-snackbar').should('have.length', 1);
   });
 });
