a0e96c1c56a966c22e180ddca480d5e3822db525
Jinbi Jin <jinbijin@jinbijin.nl>
2020-03-21 21:43:40 +0100

    Finish setting up scoring step

--- src/app/event-manager/components/steps/event-scoring-step/event-scoring-step.component.ts
+++ src/app/event-manager/components/steps/event-scoring-step/event-scoring-step.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RoundResult } from 'src/app/instrumentation/types/round-result.type';
 
 @Component({
   selector: 'agari-event-scoring-step',
@@ -7,4 +8,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class EventScoringStepComponent {
   @Input() public index: number;
+  @Input() public roundResult: RoundResult;
 }
