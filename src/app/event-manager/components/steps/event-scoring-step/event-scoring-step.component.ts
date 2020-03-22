7c45d30527f2f3450a72a132e922d3a86e25d8d8
Jinbi Jin <jinbijin@jinbijin.nl>
2020-03-22 19:46:28 +0100

    Write mapper

--- src/app/event-manager/components/steps/event-scoring-step/event-scoring-step.component.ts
+++ src/app/event-manager/components/steps/event-scoring-step/event-scoring-step.component.ts
@@ -2,8 +2,10 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { EventManagerState } from 'src/app/event-manager/store/event-manager.state';
 import { GameResult } from 'src/app/instrumentation/types/game-result.type';
+import { ParticipantResult } from 'src/app/instrumentation/types/participant-result.type';
 import { Participant } from 'src/app/instrumentation/types/participant.type';
 import { RoundResult } from 'src/app/instrumentation/types/round-result.type';
 
@@ -34,14 +36,63 @@ export class EventScoringStepComponent {
   }
 
   public setScore(gameIndex: number, game: GameResult): void {
-    this.dialog.open(ScoringDialogComponent, {
+    const dialogRef = this.dialog.open(ScoringDialogComponent, {
       data: { index: { roundIndex: this.index, gameIndex }, game }
     });
+    dialogRef
+      .afterClosed()
+      .pipe(
+        map(value => this.mapValues(value)),
+        tap(value => console.log(value))
+      )
+      .subscribe();
   }
 
   public unsetScore(gameIndex: number): void {}
 
   public trackGameByIndex(index: number, item: GameResult): number {
     return index;
+  }
+
+  private mapValues(value: any): GameResult {
+    const keys = Object.keys(value.basicScore);
+    const place = this.mapRatings(value);
+    const obj = {};
+    for (const key of keys) {
+      obj[key] = {
+        basicScoreTimesSixty: Math.round(value.basicScore[key] * 60),
+        placementScoreTimesSixty: place[key],
+        finalScoreTimesSixty: Math.round(value.basicScore[key] * 60) + place[key],
+        bonusScoreTimesSixty: Math.round(value.bonusScore[key] * 60)
+      };
+    }
+    return obj;
+  }
+
+  private mapRatings(value: any): any {
+    const keys = Object.keys(value.basicScore);
+    const obj = {};
+    for (const key of keys) {
+      obj[key] = this.placementBonus(value, key);
+    }
+    return obj;
+  }
+
+  private placementBonus(value: any, key: string): number {
+    const keys = Object.keys(value.basicScore);
+    return keys
+      .filter(k => k !== key)
+      .map(k => {
+        const score = Math.round(value.basicScore[key] * 10);
+        const opponentScore = Math.round(value.basicScore[k] * 10);
+        if (opponentScore < score) {
+          return 300;
+        } else if (opponentScore === score) {
+          return 0;
+        } else {
+          return -300;
+        }
+      })
+      .reduce((acc, val) => acc + val, 0);
   }
 }
