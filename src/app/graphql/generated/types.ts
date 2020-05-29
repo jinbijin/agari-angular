a92a42dd42d04a98d266c0342aed5d6273b62cd7
Jinbi Jin <jinbijin@jinbijin.nl>
2020-05-29 21:50:17 +0200

    Update packages

--- src/app/graphql/generated/types.ts
+++ src/app/graphql/generated/types.ts
@@ -6,18 +6,24 @@ export type Maybe<T> = T | null;
 
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
-  Date: any,
-  DateTime: any,
-  DateTimeOffset: any,
-  Seconds: any,
-  Milliseconds: any,
-  Decimal: any,
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Date` scalar type represents a year, month and day in accordance with the [ISO-8601](https://en.wikipedia.org/wiki/ISO_8601) standard. */
+  Date: any;
+  /** The `DateTime` scalar type represents a date and time. `DateTime` expects timestamps to be formatted in accordance with the [ISO-8601](https://en.wikipedia.org/wiki/ISO_8601) standard. */
+  DateTime: any;
+  /** The `DateTimeOffset` scalar type represents a date, time and offset from UTC. `DateTimeOffset` expects timestamps to be formatted in accordance with the [ISO-8601](https://en.wikipedia.org/wiki/ISO_8601) standard. */
+  DateTimeOffset: any;
+  /** The `Seconds` scalar type represents a period of time represented as the total number of seconds. */
+  Seconds: any;
+  /** The `Milliseconds` scalar type represents a period of time represented as the total number of milliseconds. */
+  Milliseconds: any;
+  Decimal: any;
 };
+
 
 
 
@@ -27,35 +33,40 @@ export type Scalars = {
 
 
 export type Query = {
-   __typename?: 'Query',
-  generateSchedule: Schedule,
+  __typename?: 'Query';
+  generateSchedule: Schedule;
 };
 
 
 export type QueryGenerateScheduleArgs = {
-  roundCount: Scalars['Int'],
-  participantCount: Scalars['Int']
+  roundCount: Scalars['Int'];
+  participantCount: Scalars['Int'];
 };
 
+/** A mahjong schedule */
 export type Schedule = {
-   __typename?: 'Schedule',
-  rounds: Array<ScheduleRound>,
+  __typename?: 'Schedule';
+  /** The rounds in the schedule */
+  rounds: Array<ScheduleRound>;
 };
 
-export type ScheduleGame = {
-   __typename?: 'ScheduleGame',
-  participantNrs: Array<Scalars['Int']>,
-};
-
+/** A round in a mahjong schedule */
 export type ScheduleRound = {
-   __typename?: 'ScheduleRound',
-  games: Array<ScheduleGame>,
+  __typename?: 'ScheduleRound';
+  /** The games in the round */
+  games: Array<ScheduleGame>;
 };
 
+/** A game in a mahjong schedule */
+export type ScheduleGame = {
+  __typename?: 'ScheduleGame';
+  /** The numbers of the participants in the game */
+  participantNrs: Array<Scalars['Int']>;
+};
 
 export type GenerateScheduleQueryVariables = {
-  roundCount: Scalars['Int'],
-  participantCount: Scalars['Int']
+  roundCount: Scalars['Int'];
+  participantCount: Scalars['Int'];
 };
 
 
