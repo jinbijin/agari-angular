7efad5069d2cb8d37caeb819c4674605aea6e532
Jinbi Jin <jinbijin@jinbijin.nl>
2020-07-26 13:10:29 +0000

    Upgrade npm packages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
 
 import { AgariModule } from './app/agari.module';
 import { environment } from './environments/environment';
