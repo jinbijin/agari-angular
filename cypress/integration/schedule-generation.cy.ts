752c611e727837ac2c001bc3236147a312e6c47c
Jinbi Jin <jinbijin@jinbijin.nl>
2021-06-27 21:27:55 +0000

    Clean up cypress tests

--- cypress/integration/schedule-generation.cy.ts
+++ cypress/integration/schedule-generation.cy.ts
@@ -1,7 +1,7 @@
 describe('Schedule generator', () => {
   it('should generate a schedule', () => {
-    cy.server();
-    cy.route('POST', 'http://localhost:5001/graphql', 'fixture:generate-schedule/response-success.json');
+    cy.fixture('generate-schedule/response-success.json').then(fixture => cy.intercept('POST', 'http://localhost:5001/schedule/round-robin', fixture));
+    cy.intercept('POST', 'http://localhost:5001/schedule/round-robin/validate', { data: true, error: null });
 
     cy.visit('http://localhost:4242/schedule-generator');
 
@@ -10,22 +10,5 @@ describe('Schedule generator', () => {
     cy.get('[data-test-id="request-button"]').click();
 
     cy.get('[data-test-id="schedule-round"]').should('have.length', 4);
-  });
-
-  it('should show a snackbar if no schedule is generated', () => {
-    cy.server();
-    cy.route(
-      'POST',
-      'http://localhost:5001/graphql',
-      'fixture:generate-schedule/response-graphql-error.json'
-    );
-
-    cy.visit('http://localhost:4242/schedule-generator');
-
-    cy.get('[data-test-id="round-input"]').type('9');
-    cy.get('[data-test-id="participant-input"]').type('36');
-    cy.get('[data-test-id="request-button"]').click();
-
-    cy.get('.mat-simple-snackbar').should('have.length', 1);
   });
 });
