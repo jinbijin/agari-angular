72d7eb2c1c7b981fa6891743f544bfde46f4e185
Jinbi Jin <jinbijin@jinbijin.nl>
2021-07-13 22:05:26 +0000

    Fix cypress tests

--- cypress/integration/schedule-generation.cy.ts
+++ cypress/integration/schedule-generation.cy.ts
@@ -1,12 +1,15 @@
 describe('Schedule generator', () => {
   it('should generate a schedule', () => {
-    cy.fixture('generate-schedule/response-success.json').then(fixture => cy.intercept('POST', 'http://localhost:5001/schedule/round-robin', fixture));
-    cy.intercept('POST', 'http://localhost:5001/schedule/round-robin/validate', { data: true, error: null });
+    cy.fixture('generate-schedule/response-success.json').then(fixture => cy.intercept('GET', 'http://localhost:5001/schedule/round-robin?participantCount=20&roundCount=4', fixture));
+    cy.intercept('GET', 'http://localhost:5001/schedule/round-robin/validate*', { data: true, error: null });
+    cy.intercept('GET', 'http://localhost:5001/schedule/round-robin/max-rounds*', { data: 5, error: null });
 
     cy.visit('http://localhost:4242/schedule-generator');
 
-    cy.get('[data-test-id="round-input"]').type('4');
     cy.get('[data-test-id="participant-input"]').type('20');
+    cy.get('[data-test-id="round-select"]').click();
+    cy.get('mat-option').contains('4').click();
+
     cy.get('[data-test-id="request-button"]').click();
 
     cy.get('[data-test-id="schedule-round"]').should('have.length', 4);
