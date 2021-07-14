1f6de2e1b1dce0b612e0c5bdd7b1cff67c296afd
Jinbi Jin <jinbijin@jinbijin.nl>
2021-07-14 20:34:18 +0000

    Alter notification setup

--- src/app/core/services/update-notification.service.ts
+++ src/app/core/services/update-notification.service.ts
@@ -6,10 +6,7 @@ import { SwUpdate } from '@angular/service-worker';
 @Injectable()
 export class UpdateNotificationService {
   public constructor(updates: SwUpdate, snackBar: MatSnackBar, @Inject(DOCUMENT) document: Document) {
-    updates.available.subscribe(event => {
-      const snackBarRef = snackBar.open('An update to a new version is available.', 'Update');
-      snackBarRef.onAction().subscribe(() => updates.activateUpdate().then(() => document.location.reload()))
-    });
-    updates.activated.subscribe(event => snackBar.open(`Update to version ${event.current} has succeeded!`, undefined, { duration: 5000 }));
+    updates.available.subscribe(event => snackBar.open(`An update to a new version (${event.available}) is available. Refresh the page to perform the update.`, 'Got it!'));
+    updates.activated.subscribe(event => snackBar.open(`Update successful, welcome to version ${event.current}!`, undefined, { duration: 5000 }));
   }
 }
