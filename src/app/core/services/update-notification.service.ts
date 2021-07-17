32df9ab1f37010cb4de7b6627ed764163126505a
jinbijin <35583057+jinbijin@users.noreply.github.com>
2021-07-17 13:01:15 +0200

    Fix update notification texts (#24)

--- src/app/core/services/update-notification.service.ts
+++ src/app/core/services/update-notification.service.ts
@@ -6,7 +6,7 @@ import { SwUpdate } from '@angular/service-worker';
 @Injectable()
 export class UpdateNotificationService {
   public constructor(updates: SwUpdate, snackBar: MatSnackBar, @Inject(DOCUMENT) document: Document) {
-    updates.available.subscribe(event => snackBar.open(`An update to a new version (${event.available}) is available. Refresh the page to perform the update.`, 'Got it!'));
-    updates.activated.subscribe(event => snackBar.open(`Update successful, welcome to version ${event.current}!`, undefined, { duration: 5000 }));
+    updates.available.subscribe(event => snackBar.open(`An update to a new version is available. Refresh the page to perform the update.`, 'Got it!'));
+    updates.activated.subscribe(event => snackBar.open(`Update successful!`, undefined, { duration: 5000 }));
   }
 }
