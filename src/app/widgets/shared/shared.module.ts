284979c04773429b30610cd7ac8b96a6770139c1
Jinbi Jin <jinbijin@jinbijin.nl>
2021-08-15 14:35:48 +0000

    Add ReactiveFormsModule to new SharedModule

--- src/app/widgets/shared/shared.module.ts
+++ src/app/widgets/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
+    ReactiveFormsModule,
   ],
   exports: [
     CommonModule,
     LayoutModule,
+    ReactiveFormsModule,
   ]
 })
 export class SharedModule {}
